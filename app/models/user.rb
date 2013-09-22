9c4e8506e27c6684cdf93be87cc93b7e4d9159d8
Blane Cordes <blane.cordes@echostar.com>
2013-09-22 01:51:25 -0400

    i suck

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :user_id
   # attr_accessible :title, :body
 
-  has_many :posts
+  has_many :posts, foreign_key: :id
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
 end
